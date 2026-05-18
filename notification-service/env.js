81f9f690c3d388b4a37706774ad4af6d35eb0f85
rafiya618 <rafiyam007@gmail.com>
2026-05-18 23:04:25 +0500

    fix variable load

--- notification-service/env.js
+++ notification-service/env.js
@@ -3,16 +3,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const SOCKET_GATEWAY_URL =
-  process.env.SOCKET_GATEWAY_URL || "http://localhost:4000";import dotenv from "dotenv";
-
-dotenv.config();
-
-export function getRequiredUrl(name) {
-  const value = process.env[name];
-
-  if (!value) {
-    throw new Error(`[notification-service] Missing required environment variable: ${name}`);
-  }
-
-  return value;
-}
+  process.env.SOCKET_GATEWAY_URL || "http://localhost:4000";
